3fcf40b9b70bc753d8a479112f12e33d59708e38
Lochlann O Neill <47467279+Lochlannoneill@users.noreply.github.com>
2022-02-26 03:14:26 +0000

    added carousels to experiments.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//tabs
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
@@ -14,3 +15,31 @@ tabs.forEach(tab => {
         target.classList.add('active')
     })
 })
+
+
+//carousels
+var $item = $('.carousel-item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
