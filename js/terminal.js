fa0a58dddbaa8ec7dd76cb3f54a42b11dfba057d
Lochlann O Neill <47467279+Lochlannoneill@users.noreply.github.com>
2022-08-14 04:49:04 +0100

    backend working now
    needs more funcionality though

--- js/terminal.js
+++ js/terminal.js
@@ -1,40 +1,45 @@
-
+const BASH = ["help", "print", "echo", "cd", "ls"];
+const SINGLES = ["ls"];
 
 function executeCommand() {
   var command = document.getElementById('command').innerHTML;
+  console.log("command: " + command);
   document.getElementById("command").innerHTML = "";
   document.getElementById("command").focus();
-  console.log("command: " + command);
   
+  //get the command type from the first word
   commandType = command.split(' ')[0];
   console.log("commandType: " + commandType);
   
-  //verify if the command is legit
+  //verify if the command is legit and execute
   var verification = verifyCommand(commandType);
-  if (verification === true) {
-    document.getElementById("terminal-response").innerHTML = (removeFirstWord(command));
-    // executeAppropriateCommand(command);
+  console.log("acceptable: " + verification);
+  if (verification) {
+    var success = (executeAppropriateCommand(commandType, removeFirstWord(command)))
+    console.log("success: " + success);
+    document.getElementById("terminal-response").innerHTML = success;
   } else {
     document.getElementById("terminal-response").innerHTML = ("'" + commandType + "' is not a valid command type");
   }
   
-  console.log("commandType: " + commandType);
-  console.log("acceptable: " + verification);
 }
 
-function executeAppropriateCommand(commandType, command) {
-  
+function executeAppropriateCommand(commandType, commandWithoutType) {
+  if (commandType === 'help') return "Try some of these commands: [" + BASH.join(', ') + "]";
+  if (commandType === 'print') return commandWithoutType;
+  if (commandType === 'echo') return commandWithoutType;
+  if (commandType === 'cd') return commandWithoutType.toLowerCase() + ".html";
+  if (commandType === 'ls') return "Why do you want to look at my files? They're private!"
 }
 
 //return boolean if command is valid
 function verifyCommand(commandType) {
-  if (commandType.includes("help", "print", "echo", "cd")) {
-    return true;
-  }
+  if (BASH.includes(commandType)) return true;
   return false;
 }
 
 //return string without first word
 function removeFirstWord(str) {
-  return str.substr(str.indexOf(" ") + 1);
+  if (str.trim().indexOf(' ') != -1) return str.substr(str.indexOf(" ") + 1);
+  return str;
 }
