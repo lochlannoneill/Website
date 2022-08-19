e806f5d26ca10e5b836f800d50cfb7b9be589f33
Lochlann O Neill <47467279+Lochlannoneill@users.noreply.github.com>
2022-08-19 02:26:18 +0100

    home corner link + minor random styling

--- js/carousel.js
+++ js/carousel.js
@@ -24,7 +24,8 @@ var bgMusicControls = true; // Show UI music control
 
 // ===================== start =======================
 // animation start after 1000 miliseconds
-setTimeout(init, 1000);
+setTimeout(init, 0);
+// setTimeout(init, 1000);
 
 var odrag = document.getElementById('drag-container');
 var ospin = document.getElementById('spin-container');
@@ -38,8 +39,8 @@ ospin.style.height = imgHeight + "px";
 
 // Size of ground - depend on radius
 var ground = document.getElementById('ground');
-ground.style.width = radius * 12 + "px";
-ground.style.height = radius * 12 + "px";
+ground.style.width = radius * 3 + "px";
+ground.style.height = radius * 3 + "px";
 
 function init(delayTime) {
   for (var i = 0; i < aEle.length; i++) {
@@ -49,23 +50,23 @@ function init(delayTime) {
   }
 }
 
-function applyTranform(obj) {
-  // Constrain the angle of camera (between 0 and 180)
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
+// function applyTranform(obj) {
+//   // Constrain the angle of camera (between 0 and 180)
+//   if(tY > 180) tY = 180;
+//   if(tY < 0) tY = 0;
 
-  // Apply the angle
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
-}
+//   // Apply the angle
+//   obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+// }
 
-function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
-}
+// function playSpin(yes) {
+//   ospin.style.animationPlayState = (yes?'running':'paused');
+// }
 
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
+// var sX, sY, nX, nY, desX = 0,
+//     desY = 0,
+//     tX = 0,
+//     tY = 10;
 
 // auto spin
 if (autoRotate) {
@@ -74,13 +75,13 @@ if (autoRotate) {
 }
 
 // add background music
-if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
-<p>If you are reading this, it is because your browser does not support the audio element.</p>
-</audio>
-`;
-}
+// if (bgMusicURL) {
+//   document.getElementById('music-container').innerHTML += `
+// <audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
+// <p>If you are reading this, it is because your browser does not support the audio element.</p>
+// </audio>
+// `;
+// }
 
 // setup events
 // document.onpointerdown = function (e) {
